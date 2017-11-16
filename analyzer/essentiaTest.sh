9fa238a26c7ccfd72ebf0f7802da6e4627e76c67
Zach DeCook <zachdecook@gmail.com>
2017-11-16 14:56:28 -0500

    - Essentia Analyzer: Clean up script

--- analyzer/essentiaTest.sh
+++ analyzer/essentiaTest.sh
@@ -11,18 +11,9 @@ if [ -h "./standard_pitchdemo" ]
 then
 	echo ""
 else
-	read -p "Run the cron script now? " answer
-	echo "$file not found."
+	echo "Please place the file standard_pitchdemo in the working directory"
 fi
 
 # 4 is multi pitch estimation Melodia
 ./standard_pitchdemo $1 file.csv 4
 cat file.csv
-
-# Found formula online
-# freq = (440. * math.exp(.057762265 * (midi - 69.)));
-# f = 440 * e^{.057762265 * (m - 69)}
-# solved by wolfram alpha
-# m = 17.3123 log(0.122312 f) for f>0
-# midi = 17.3123 ln(0.122312 * freq )
-

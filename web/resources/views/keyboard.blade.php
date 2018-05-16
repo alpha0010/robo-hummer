bdf23f8415bed0da18c741a0426915935b3c1638
Your Name <robodrummer687@gmail.com>
2018-05-16 15:08:37 -0400

    * Home Page: clean up style - don't use absolute positioning

--- web/resources/views/keyboard.blade.php
+++ web/resources/views/keyboard.blade.php
@@ -1,45 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<!--	<object id='Jazz1' classid='CLSID:1ACE1618-1C7D-4561-AEE1-34842AA85E90' class='hidden' style='display:none'>
-			<object id='Jazz2' type='audio/x-jazz' class='hidden'></object>
-		</object>
-		-->
-<div class='content'>
-		<!--
-		<h5>Works best in Chrome.
-		For other browser support, consider installing the <a href='http://jazz-soft.net/download/Jazz-Plugin/'>Jazz-Plugin</a>
-	</h5>
-<a href='#' onclick='setup();'>Relink</a>
-	<p id='status'></p>
-	<div id='dropdowns'></div>-->
+<div class='container'>
+	<h2>{{ config('app.name') }} Melody Search</h2>
+
+	<p>
+		Search for a song by entering a snippet of its melody. Either click on
+		the virtual keyboard, or type the letters displayed. Search does take
+		into account both note pitch and duration, but is forgiving as long as
+		your are <em>close enough</em>.
+	</p>
+	<p>
+		Click the box below to enable typing input.
+	</p>
 
 	<div class='keyboard'>
-	<a href='#' data-midi='60' class='WhiteKey'>A</a>
+	<a href='#' data-midi='60' class='WhiteKey'><span>A</span></a>
 	<a href='#' data-midi='61' class='BlackKey cs'>W</a>
-	<a href='#' data-midi='62' class='WhiteKey'>S</a>
+	<a href='#' data-midi='62' class='WhiteKey'><span>S</span></a>
 	<a href='#' data-midi='63' class='BlackKey ds'>E</a>
-	<a href='#' data-midi='64' class='WhiteKey'>D</a>
-	<a href='#' data-midi='65' class='WhiteKey '>F</a>
+	<a href='#' data-midi='64' class='WhiteKey'><span>D</span></a>
+	<a href='#' data-midi='65' class='WhiteKey '><span>F</span></a>
 	<a href='#' data-midi='66' class='BlackKey fs'>T</a>
-	<a href='#' data-midi='67' class='WhiteKey'>G</a>
+	<a href='#' data-midi='67' class='WhiteKey'><span>G</span></a>
 	<a href='#' data-midi='68' class='BlackKey gs'>Y</a>
-	<a href='#' data-midi='69' class='WhiteKey'>H</a>
+	<a href='#' data-midi='69' class='WhiteKey'><span>H</span></a>
 	<a href='#' data-midi='70' class='BlackKey as'>U</a>
-	<a href='#' data-midi='71' class='WhiteKey'>J</a>
-	<a href='#' data-midi='72' class='WhiteKey'>K</a>
+	<a href='#' data-midi='71' class='WhiteKey'><span>J</span></a>
+	<a href='#' data-midi='72' class='WhiteKey'><span>K</span></a>
 	<a href='#' data-midi='73' class='BlackKey cs'>O</a>
-	<a href='#' data-midi='74' class='WhiteKey'>L</a>
+	<a href='#' data-midi='74' class='WhiteKey'><span>L</span></a>
 	<a href='#' data-midi='75' class='BlackKey ds'>P</a>
-	<a href='#' data-midi='76' class='WhiteKey'>;</a>
-	<a href='#' data-midi='77' class='WhiteKey'>'</a><!--
-	<a href='#' data-midi='78' class='BlackKey'></a>
-	<a href='#' data-midi='79' class='WhiteKey'></a>
-	<a href='#' data-midi='80' class='BlackKey'></a>
-	<a href='#' data-midi='81' class='WhiteKey'></a>
-	<a href='#' data-midi='82' class='BlackKey'></a>
-	<a href='#' data-midi='83' class='WhiteKey'></a>
-	<a href='#' data-midi='84' class='WhiteKey'></a>-->
+	<a href='#' data-midi='76' class='WhiteKey'><span>;</span></a>
+	<a href='#' data-midi='77' class='WhiteKey'><span>'</span></a>
 	<form><input type='text'></input></form>
 	</div>
 
@@ -50,7 +43,7 @@
 		<span id="bpmArea"></span>
 	</form>-->
 	<pre id='csv' style='font-size: .5em;'></pre>
-	<button id='searchCSV' class='btn btn-primary'>Search CSV</button>
+	<button id='searchCSV' class='btn btn-primary'>Search</button>
 	<div id='results'></div>
 </div>
 

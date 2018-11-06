0f4f547e00101926835f8e7008b0238717fee9ec
Zach DeCook <zdecook@ccel.org>
2018-11-06 13:47:49 -0500

    - Create Media Test: Test that the file is actually there

--- web/tests/Feature/CreateMediaTest.php
+++ web/tests/Feature/CreateMediaTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use Artisan;
+use App\Media;
 use Tests\ClearMedia;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
 
 class CreateMediaTest extends TestCase
 {
@@ -18,15 +20,20 @@ class CreateMediaTest extends TestCase
 	 */
 	public function testCreateMediaFromLocal()
 	{
+		$localPath = "../examplemedia/1/melody.musicxml";
 		$exitCode = Artisan::call(
 			"media:create",
-			[ 'file' => "../examplemedia/1/melody.musicxml" ]
+			[ 'file' => $localPath ]
 		);
 		$this->assertEquals( 0, $exitCode );
 		$this->assertDatabaseHas( "media",
 			[
 				"originalFile" => "melody.musicxml"
 			]
+		);
+		$this->assertEquals(
+			Storage::get( Media::getDir() . "/1/melody.musicxml" ),
+			file_get_contents( $localPath )
 		);
 	}
 }

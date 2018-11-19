af8bdf84cf2cfb9e86bdd93538a0bdfc57177e59
Zach DeCook <zdecook@ccel.org>
2018-11-19 13:07:19 -0500

    - Music 21 Stream: Write midi to temporary file before spitting it out

--- tools/convert.py
+++ tools/convert.py
@@ -2,6 +2,7 @@
 from music21 import *
 
 import sys
+import os
 
 filename = sys.argv[1]
 outputformat = sys.argv[2]
@@ -11,10 +12,11 @@ if ( outputformat == 'harmony.musicxml' ):
 	s.write( 'xml', '/dev/stdout' )
 elif( outputformat == 'harmony.midi' ):
 	s = converter.parse(filename)
-	s.write( 'midi', '/tmp/file.midi' )
-	import sys
+	# Creates a temporary file
+	path = s.write( 'midi' )
 	# Read file as bytes, write as buffer
 	# so python's io.TextIOBase doesn't try to decode these bytes into ASCII.
-	sys.stdout.buffer.write( open( '/tmp/file.midi', 'rb' ).read() )
+	sys.stdout.buffer.write( open( path, 'rb' ).read() )
+	os.remove( path )
 elif( outputformat == 'incipit.json' ):
 	import incipit

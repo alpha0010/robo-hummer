cc17c1f5c121da1462650eba77725ea5a572a85f
Zach DeCook <zdecook@ccel.org>
2018-12-03 12:20:21 -0500

    * Post Media: Write more thorough tests for checking if JWT and post request were valid

--- web/tests/Feature/PostMediaTest.php
+++ web/tests/Feature/PostMediaTest.php
@@ -20,8 +20,19 @@ class PostMediaTest extends TestCase
 	use ClearMedia;
 	use ClearKeys;
 
+	public function testPostMediaNotSetup()
+	{
+		$file = new UploadedFile( '/var/www/examplemedia/2/melody.midi', 'filename.ext' );
+		$response = $this->post( '/api/media', [ 'file' => $file ] );
+
+		$response
+			->assertStatus( 500 )
+			->assertSee( "Trusted key not set up properly." );
+	}
+
 	public function testPostMediaUnAuthenticated()
 	{
+		$this->setupTestKeys();
 		$file = new UploadedFile( '/var/www/examplemedia/2/melody.midi', 'filename.ext' );
 		$response = $this->post( '/api/media', [ 'file' => $file ] );
 
@@ -29,10 +40,37 @@ class PostMediaTest extends TestCase
 			->assertStatus( 403 );
 	}
 
+	public function testPostMediaMangledJWT()
+	{
+		$this->setupTestKeys();
+		$file = new UploadedFile( '/var/www/examplemedia/2/melody.midi', 'filename.ext' );
+		$response = $this->post( '/api/media', [ 'file' => $file, 'jwt' => 'not a jwt string' ] );
+
+		$response
+			->assertStatus( 403 );
+	}
+
+	public function testPostMediaUnTrustedUUID()
+	{
+		$this->setupTestKeys();
+		$file = new UploadedFile( '/var/www/examplemedia/2/melody.midi', 'filename.ext' );
+		$response = $this->post( '/api/media',
+			[ 'file' => $file, 'jwt' => $this->getJWT( 'untrusted-uuid' ) ]
+		);
+
+		$response
+			->assertStatus( 403 );
+	}
+
 	public function testPostMedia()
 	{
+		$this->setupTestKeys();
+		// Create trusted user
+		$uuid = 'ab8bc5f3-3bc7-487d-a5bf-0a542caae79f';
+		Artisan::call( "robo:trust-uuid", [ 'uuid' => $uuid ] );
+
 		$file = new UploadedFile( '/var/www/examplemedia/2/melody.midi', 'filename.ext' );
-		$response = $this->post( '/api/media', [ 'file' => $file, 'jwt' => $this->getJWT() ] );
+		$response = $this->post( '/api/media', [ 'file' => $file, 'jwt' => $this->getJWT( $uuid ) ] );
 
 		$response
 			->assertJson( [ 'textID' => NULL, 'tuneID' => NULL ] )
@@ -44,11 +82,16 @@ class PostMediaTest extends TestCase
 
 	public function testPostMediaWithIDs()
 	{
+		$this->setupTestKeys();
+		// Create trusted user
+		$uuid = 'ab8bc5f3-3bc7-487d-a5bf-0a542caae79f';
+		Artisan::call( "robo:trust-uuid", [ 'uuid' => $uuid ] );
+
 		$originalFile = '/var/www/examplemedia/1/melody.musicxml';
 		$file = new UploadedFile( $originalFile, 'filename.ext' );
 		$response = $this->post(
 			'/api/media',
-			[ 'file' => $file, 'textID' => 12345, 'tuneID' => 54321, 'jwt' => $this->getJWT() ]
+			[ 'file' => $file, 'textID' => 12345, 'tuneID' => 54321, 'jwt' => $this->getJWT( $uuid ) ]
 		);
 
 		$response
@@ -64,9 +107,9 @@ class PostMediaTest extends TestCase
 	}
 
 	/**
-	 * @brief returns a JWT one can use for posting files.
+	 * @brief Install a public key (and store the private key so we can use it for testing).
 	 */
-	private function getJWT()
+	private function setupTestKeys()
 	{
 		// Generate Key pair
 		$privateKey = 'testing-sso-private.key';
@@ -75,11 +118,15 @@ class PostMediaTest extends TestCase
 
 		$publicKeyContents = shell_exec( "(cat $privatePath | openssl rsa -pubout)2>/dev/null" );
 		$exitCode = Artisan::call( "robo:set-sso-key", [ 'key' => $publicKeyContents ] );
+	}
 
-		// Create trusted user
-		$uuid = 'ab8bc5f3-3bc7-487d-a5bf-0a542caae79f';
-		Artisan::call( "robo:trust-uuid", [ 'uuid' => $uuid ] );
-
+	/**
+	 * @brief returns a JWT one can use for posting files.
+	 * @param $uuid The UUID who this should be coming from.
+	 */
+	private function getJWT( $uuid )
+	{
+		$privatePath = storage_path( "app/testing-sso-private.key" );
 		// Create JWT.
 		$keychain = new Keychain();
 		$builder = new Builder();

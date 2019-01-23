11ac5a07b55cfc03d5ee422c33856af084af5a9c
Zach DeCook <zdecook@ccel.org>
2019-01-23 10:52:54 -0500

    * Dynamic Presentation: Break slides at measure breakpoints

--- web/app/Http/Controllers/DynamicController.php
+++ web/app/Http/Controllers/DynamicController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client as HttpClient;
+
 class DynamicController extends Controller
 {
     /**
@@ -22,16 +24,53 @@ class DynamicController extends Controller
     {
         $slides = [[]];
 
-        // TODO: Calculate the number of slides "wide" an individual verse should be.
-        $number = 6;
+        $client = new HttpClient();
+        $resp = (string)$client->get( route("get media", [$id, "dynamic.svg.info.json"]))->getBody();
+        $jsonArray = json_decode($resp, TRUE);
+
+        // 960px seems to be hard-coded into reveal.js as the 'screen width',
+        // even scaled down for smaller screens, so moving 960px forward will
+        // never skip too far forward, beyond what has already been seen.
+        $screenWidth = 960;
+        $offsetBreaks = $this->getOffsetBreaks($jsonArray['measureOffsets'], $screenWidth);
 
         // TODO: Load each verse into a different section.
         $file = route("get media", [$id, "dynamic.svg"]);
-        for ($i = 0; $i < $number; $i++) {
-            $slides[0][] = $this->render("Verse 1 slide $i", $file, $i);
+        $i = 0;
+        foreach ($offsetBreaks as $offset) {
+            $slides[0][] = $this->render("Verse 1 slide $i", $file, $offset);
+            $i++;
         }
 
         return view("slides", ["slides" => $slides]);
+    }
+
+    /**
+     * @brief Go through each of the $measureOffsets,
+     *  and create breakpoints at the measures that are less than $screenWidth apart.
+     * @param array $measureOffsets An array of x positions where measure breaks are made.
+     * @param int $screenWidth The maximum space between one breakpoint and another.
+     * @return array Where the slides should break at.
+     */
+    private function getOffsetBreaks( $measureOffsets, $screenWidth )
+    {
+        // Where we should break each slide at.
+        $offsetBreaks = [];
+        $current = 0;
+        // Go through each of the measureOffsets,
+        // and use them as breaks that are less than $screenWidth apart.
+        foreach ( $measureOffsets as $number => $measureOffset )
+        {
+            if ( $current == 0 ) {
+                $offsetBreaks[] = $measureOffset;
+                $current++;
+            } else if ($offsetBreaks[$current - 1] + $screenWidth <= $measureOffset) {
+                // If the current offset is too far away, break at the previous offset.
+                $offsetBreaks[] = $measureOffsets[$number - 1];
+                $current++;
+            }
+        }
+        return $offsetBreaks;
     }
 
     /**
@@ -39,15 +78,12 @@ class DynamicController extends Controller
      *
      * @return array
      */
-    private function render(string $slideName, string $file, $number)
+    private function render(string $slideName, string $file, int $offset)
     {
-        // 960px seems to be hard-coded into reveal.js as the 'screen width',
-        // even scaled down for smaller screens, so moving 960px forward will
-        // never skip too far forward, beyond what has already been seen.
-        $offset = 0 - (960 * $number);
+        $offset = 0 - $offset;
         return [
             "name"    => $slideName,
-            "content" => "<img class='dynamic' style='margin-left:{$offset}px;' src='$file'/>",
+            "content" => "<img class='dynamic' style='margin-left:{$offset}px;' src='{$file}'/>",
         ];
     }
 }

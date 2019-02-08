fd9c2e4a61c5286afdf0d72b9c8df912404be3cc
Zach DeCook <zdecook@ccel.org>
2019-02-08 11:26:28 -0500

    * Note Size: Create function to change note size

--- tools/dynamic.py
+++ tools/dynamic.py
@@ -6,7 +6,8 @@ from xml.sax.saxutils import escape as XMLescape
 # How wide a quarter note is.
 xScale = 84
 # How tall a semitone is.
-yScale = 20
+defaultYScale = 20
+defaultFontSize = 18
 
 border = 1
 colors = ['red', 'green', 'blue', 'yellow', 'cyan', 'magenta']
@@ -24,27 +25,37 @@ def rectangle(x, y, w, h, lyrics, color, lyricLineY):
         lyricLineY is an alternate Y value placement for the lyrics rather than on the rectangle.
             `data-y` is the Y value for being on the rectangle, `data-y-bottom` is the alternate placement.
     """
-    x = x * xScale
-    w = w * xScale
-    y = y * yScale
-    h = h * yScale
+    # Create "scaled" versions of each of the variables. The file will be rendered with data that allows
+    # different parts to be scaled.
+    sx = x * xScale
+    sw = w * xScale
+    sy = y * defaultYScale
+    sh = h * defaultYScale
     style = "fill:%s; stroke-width: %i; stroke:rgb(0,0,0); opacity: 0.5;" \
             % (color, border)
     border2 = border * 2
     print("\n")
     print("<g>")
-    print("<rect x='%i' y='%i' width='%i' height='%i' style='%s'/>"
-          % (x, y, w, h, style))
+    print("<rect x='%i' y='%i' width='%i' height='%i'"
+          % (sx, sy, sw, sh) +
+          " data-x='%f' data-y='%f' data-width='%f' data-height='%f'"
+          % (x, y, w, h) +
+          " style='%s'/>"
+          % (style)
+    )
     if lyrics:
         dataVerses = ""
         for lyric in lyrics:
             escapedText = lyric.rawText
             escapedText = XMLescape(escapedText, {"'": "&apos;"})
             dataVerses += "data-v" + str(lyric.number) + "='" + escapedText + "' "
-        lyricX = x + border
-        lyricY = y + h - border
-        print("<text x='%i' data-textlength='%i' lengthAdjust='spacingAndGlyphs' y='%i' data-y='%i' data-y-bottom='%i' font-size='%ipt' %s>"
-              % (lyricX, w, lyricY, lyricY, lyricLineY, h - border2, dataVerses))
+        lyricX = sx + border
+        lyricY = sy + sh - border
+        print("<text x='%i' data-textlength='%i' lengthAdjust='spacingAndGlyphs' "
+              % (lyricX, sw) +
+              "y='%i' data-y='%i' data-y-bottom='%i' font-size='%ipt' %s>"
+              % (lyricY, lyricY, lyricLineY, defaultFontSize, dataVerses))
+
         # TODO: use syllabic for something.
         text = XMLescape(lyrics[0].rawText)
         textBytes = text.encode('utf-8').strip()
@@ -81,9 +92,7 @@ highNote = max(s.pitches).midi
 noteRange = highNote - lowNote + 1
 
 songWidth = songLength * xScale
-# If the lyrics are shown at the bottom, we need this much space to put them there.
-lyricLine = 1.5
-songHeight = (noteRange + lyricLine) * yScale
+songHeight = (noteRange * defaultYScale) + (1.5 * defaultFontSize)
 
 measureLengths = {0: 0}
 measureOffsets = {0: 0}
@@ -116,7 +125,7 @@ for note in s.recurse().notes:
         if note.lyrics:
             lyrics = note.lyrics
 
-        lyricLineY = (noteRange + 1) * yScale
+        lyricLineY = (noteRange + 1) * defaultYScale
         rectangle(xPos, yPos, xLen, yLen, lyrics, color, lyricLineY)
 
 print("<g id='measureBarLines'>")

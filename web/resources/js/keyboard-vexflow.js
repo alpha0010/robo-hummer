7ee81525c5e0fa88163b5fa5c8966ef892fbe84d
Zach DeCook <zdecook@ccel.org>
2019-03-04 10:33:37 -0500

    - Javascript Code: Run Prettier over it

--- web/resources/js/keyboard-vexflow.js
+++ web/resources/js/keyboard-vexflow.js
@@ -1,75 +1,80 @@
 var Vex = require('vexflow');
 
-$(document).ready(function(){
-	updateStave();
-	$('#bpmSlider').change(function(val){
-		updateStave($(this).val()/0.06);
-		$('#bpmArea').text("bpm: " + $(this).val());
-	});
+$(document).ready(function() {
+  updateStave();
+  $('#bpmSlider').change(function(val) {
+    updateStave($(this).val() / 0.06);
+    $('#bpmArea').text('bpm: ' + $(this).val());
+  });
 });
 
 /**
  * @brief Make the stave match the state of the csv.
  */
-function updateStave(wholeLength = 2000){
-	var notes = "";
-	var totalTime = 0;
+function updateStave(wholeLength = 2000) {
+  var notes = '';
+  var totalTime = 0;
 
-	var ln = {
-		1 : 'w',
-		.75 : 'h.',
-		.5 : 'h',
-		.375 : 'q.',
-		.25 : 'q',
-		.1875 : '8.',
-		.125 : '8',
-		.0625 : '16',
-	};
+  var ln = {
+    1: 'w',
+    0.75: 'h.',
+    0.5: 'h',
+    0.375: 'q.',
+    0.25: 'q',
+    0.1875: '8.',
+    0.125: '8',
+    0.0625: '16'
+  };
 
-	var lines = $("#csv").text().split('\n');
-	for(var i = 0;i < lines.length;i++){
-		var note = lines[i].split(',')
-		if ( MIDI.noteToKey[note[0]] != undefined ){
-			var gamut = MIDI.noteToKey[note[0]];
-			var time = getLength(parseInt(note[1]), wholeLength)
-			var length = ln[time]
-			notes += gamut + "/" + length +  ",";
-			totalTime += time
-		}
-	}
+  var lines = $('#csv')
+    .text()
+    .split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var note = lines[i].split(',');
+    if (MIDI.noteToKey[note[0]] != undefined) {
+      var gamut = MIDI.noteToKey[note[0]];
+      var time = getLength(parseInt(note[1]), wholeLength);
+      var length = ln[time];
+      notes += gamut + '/' + length + ',';
+      totalTime += time;
+    }
+  }
 
-	$("#staveInput").text("");
+  $('#staveInput').text('');
 
-	var vf = new Vex.Flow.Factory({
-		renderer: {elementId: 'staveInput', width: 500, height: 200}
-	});
+  var vf = new Vex.Flow.Factory({
+    renderer: { elementId: 'staveInput', width: 500, height: 200 }
+  });
 
-	var score = vf.EasyScore();
-	var system = vf.System();
+  var score = vf.EasyScore();
+  var system = vf.System();
 
-	system.addStave({
-		voices: [
-			score.voice(score.notes(notes, {stem: 'up'}), {time: totalTime * 16 + '/16'}),
-		]
-	}).addClef('treble');
-	vf.draw();
+  system
+    .addStave({
+      voices: [
+        score.voice(score.notes(notes, { stem: 'up' }), {
+          time: totalTime * 16 + '/16'
+        })
+      ]
+    })
+    .addClef('treble');
+  vf.draw();
 }
-
 
 /**
  * @brief take in milliseconds and return fractions of a beat.
  */
-function getLength( milliseconds, wholeLength = 2000 ){
-	// BPM = 120bpm, 500ms is a quarter note, 2s is a whole note.
-	var fraction = milliseconds / wholeLength;
-	if ( fraction >= 1 ) return 1;
-	else if ( fraction >= .75 ) return .75;
-	else if ( fraction >= .5 ) return .5;
-	else if ( fraction >= .375 ) return .375;
-	else if ( fraction >= .25 ) return .25;
-	else if ( fraction >= .1875 ) return .1875;
-	else if ( fraction >= .125 ) return .125;
-	else return .0625;
+function getLength(milliseconds, wholeLength = 2000) {
+  // BPM = 120bpm, 500ms is a quarter note, 2s is a whole note.
+  var fraction = milliseconds / wholeLength;
+  if (fraction >= 1) return 1;
+  else if (fraction >= 0.75) return 0.75;
+  else if (fraction >= 0.5) return 0.5;
+  else if (fraction >= 0.375) return 0.375;
+  else if (fraction >= 0.25) return 0.25;
+  else if (fraction >= 0.1875) return 0.1875;
+  else if (fraction >= 0.125) return 0.125;
+  else return 0.0625;
 }
 
 window.updateStave = updateStave;
--- web/resources/js/welcome.js
+++ web/resources/js/welcome.js
@@ -1,57 +1,60 @@
-var MediaStreamRecorder = require("msr");
+var MediaStreamRecorder = require('msr');
 
 Welcome = {
-    init: function() {
-        Welcome.mediaRecorder = null;
+  init: function() {
+    Welcome.mediaRecorder = null;
 
-        jQuery("#record").click(Welcome.onRecord);
-        jQuery("#save").click(Welcome.onSave);
-    },
+    jQuery('#record').click(Welcome.onRecord);
+    jQuery('#save').click(Welcome.onSave);
+  },
 
-    onRecord: function() {
-        navigator.getUserMedia(
-            {audio: true},
-            Welcome.onMediaSuccess,
-            Welcome.onMediaError
-        );
-    },
+  onRecord: function() {
+    navigator.getUserMedia(
+      { audio: true },
+      Welcome.onMediaSuccess,
+      Welcome.onMediaError
+    );
+  },
 
-    onSave: function() {
-        if (Welcome.mediaRecorder) {
-            Welcome.mediaRecorder.stop();
-        }
-    },
-
-    onMediaSuccess: function(stream) {
-        var mediaRecorder = new MediaStreamRecorder(stream);
-        mediaRecorder.mimeType = "audio/wav";
-        mediaRecorder.ondataavailable = Welcome.uploadFile;
-
-        mediaRecorder.start(30 * 1000);
-        Welcome.mediaRecorder = mediaRecorder;
-    },
-
-    onMediaError: function(e) {
-        console.error("Media error", e);
-    },
-
-    uploadFile: function(blob) {
-        var fd = new FormData();
-        fd.append("audio", blob);
-
-        jQuery.ajax({
-            type: "POST",
-            url: "/api/upload",
-            data: fd,
-            processData: false,
-            contentType: false
-        }).done(function(data) {
-            console.log("Post success");
-            console.log(data);
-        }).fail(function() {
-            console.log("Post fail");
-        });
+  onSave: function() {
+    if (Welcome.mediaRecorder) {
+      Welcome.mediaRecorder.stop();
     }
+  },
+
+  onMediaSuccess: function(stream) {
+    var mediaRecorder = new MediaStreamRecorder(stream);
+    mediaRecorder.mimeType = 'audio/wav';
+    mediaRecorder.ondataavailable = Welcome.uploadFile;
+
+    mediaRecorder.start(30 * 1000);
+    Welcome.mediaRecorder = mediaRecorder;
+  },
+
+  onMediaError: function(e) {
+    console.error('Media error', e);
+  },
+
+  uploadFile: function(blob) {
+    var fd = new FormData();
+    fd.append('audio', blob);
+
+    jQuery
+      .ajax({
+        type: 'POST',
+        url: '/api/upload',
+        data: fd,
+        processData: false,
+        contentType: false
+      })
+      .done(function(data) {
+        console.log('Post success');
+        console.log(data);
+      })
+      .fail(function() {
+        console.log('Post fail');
+      });
+  }
 };
 
 jQuery(document).ready(Welcome.init);

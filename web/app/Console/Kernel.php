17f2160e4fcd6be7e04e8151e9d8a89727046e48
Zach DeCook <zdecook@ccel.org>
2019-03-18 16:25:50 -0400

    * Tasks: Create cron container, reindex melody search daily

--- web/app/Console/Kernel.php
+++ web/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CacheMedia;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,15 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(CacheMedia::class, ['--type' => '6-tuples.json'])
+            ->withoutOverlapping()
+            ->daily()
+            ->appendOutputTo('/var/www/web/storage/logs/cacheTuples.log');
+        $schedule->exec("find /srv/robo-media-dart -perm /o+r -name 6.tuples.json "
+            . "| sudo -u python ../tools/indexer.py /var/www/melodyindex")
+            ->withoutOverlapping()
+            ->daily()
+            ->appendOutputTo('/var/www/web/storage/logs/reindexMelody.log');
     }
 
     /**
